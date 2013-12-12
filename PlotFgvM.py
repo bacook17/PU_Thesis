a79dc1ba28d700db6634652e7b50605fb3c61ec1
bacook17 <bacook@princeton.edu>
2013-12-12 14:01:35 -0500

    PlotFgvM and figures

---
+++ PlotFgvM.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+import sys, os.path as op
+import matplotlib.pyplot as plt, numpy as np, os
+
+"""
+Plot the gas-fraction against Mass, for the cluster sample provided in inputfile
+"""
+
+def PlotFgvR(inputfile):
+    GasData = np.genfromtxt(inputfile, dtype=None, names=True)
+
+    #Radius in units of R500
+    R500 = 1.
+    R200 = 1.325
+    Rvir = 1.90
+    R3_500 = 3.
+
+    Mass = GasData['Mass']/1e+13
+
+    plt.errorbar(Mass, GasData['F500'], yerr=GasData['err500'], c='b', marker='o', ls='', label=r'R$_{500}$')
+    plt.errorbar(Mass, GasData['F200'], yerr=GasData['err200'], c='g', marker='o', ls='', label=r'R$_{200}$')
+    plt.errorbar(Mass, GasData['Fvir'], yerr=GasData['errvir'], c='r', marker='o', ls='', label=r'R$_{vir}$')
+
+def SetAxes(legend=False):
+    plt.axhline(y=0.165, ls='-', c='k', label=r'$\Omega_{b}$/$\Omega_{M}$ (WMAP)')
+    plt.axhline(y=0.153, ls='--', c='k', label=r'Expected Hot Gas')
+    plt.xlabel(r'Mass ($\times 10^{13} M_\odot$)')
+    plt.ylabel(r'F$_{gas}$ (<R)')
+
+    plt.xscale('log')
+    plt.xlim(xmin=4)
+
+    if legend:
+        plt.legend(loc=0)
+
+if __name__ == '__main__':
+    inputfile = 'F_gas.dat'
+    plt.figure(1, facecolor='w')
+    PlotFgvR(inputfile)
+    SetAxes(legend=True)
+    plt.show()
